cd190c9c80979e21e1b937d5fe08353032b5a517
Ekansh Sinha <71957034+EkanshSinha@users.noreply.github.com>
2026-02-03 23:14:29 +0530

    Add files via upload

---
+++ Day3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int n, key;
+    int comparisons = 0;
+    int foundIndex = -1;
+
+
+    scanf("%d", &n);
+
+    int arr[n];
+
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+
+    scanf("%d", &key);
+
+
+    for(int i = 0; i < n; i++) {
+        comparisons++;
+        if(arr[i] == key) {
+            foundIndex = i;
+            break;
+        }
+    }
+
+
+    if(foundIndex != -1)
+        printf("Found at index %d\n", foundIndex);
+    else
+        printf("Not Found\n");
+
+    printf("Comparisons = %d", comparisons);
+
+    return 0;
+}
